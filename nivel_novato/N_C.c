1e518f6eb53511472452aa4cb286231b354a4a74
Eby <ebytuber@gmail.com>
2026-04-17 17:20:02 -0300

    nivel 1 concluido

---
+++ nivel_novato/N_C.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+
+    // movendo a torre com for
+    printf("movendo torre:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Direita\n");
+    }
+
+    // movendo o bispo com while
+    printf("\nmovendo o bispo\n");
+    int b = 0;
+    while (b < 5)
+    {
+        printf("Cima, Direita\n");
+        b++;
+    }
+
+    // movendo a rainha com while-do
+    printf("\n movendo rainha:\n");
+    int r = 0;
+    do
+    {
+        printf("esquerda\n");
+        ++r;
+    } while (r < 8);
+
+    return 0;
+}
