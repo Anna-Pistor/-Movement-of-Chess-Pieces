b3f4c76ba186b0f8442927ce968c4b99c32f8307
Eby <ebytuber@gmail.com>
2026-04-19 18:13:20 -0300

    nivel 3 concluido

---
+++ nivel_mestre/M_C.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+void movertorre(int casas)
+{
+    if (casas > 0)
+    {
+        printf("direita\n");
+        movertorre(casas - 1);
+    }
+}
+
+void moverbispo(int casas)
+{
+    if (casas > 0)
+    {
+        for (int b = 0; b < 1; b++)
+        {
+            printf("cima,");
+        }
+        printf("direita\n");
+        moverbispo(casas - 1);
+    }
+}
+
+void moverrainha(int casas)
+{
+    if (casas > 0)
+    {
+        printf("esquerda\n");
+        moverrainha(casas - 1);
+    }
+}
+
+int main()
+{
+
+    // movendo a torre
+    printf("movendo torre:\n");
+    movertorre(5);
+
+    // movendo o bispo com for alinhado
+    printf("\nmovendo o bispo:\n");
+    moverbispo(5);
+
+    // movendo a rainha com while-do
+    printf("\nmovendo a rainha:\n");
+    moverrainha(8);
+
+    // movendo o cavalo em L com for e while
+
+    printf("\nmovendo o cavalo:\n");
+    for (int c = 0; c < 2; c++)
+    {
+        printf("cima,");
+
+        if (c < 1)
+        {
+            continue;
+        }
+        for (int v = 0; v < 1; v++)
+        {
+            printf("direita\n");
+        }
+    }
+
+    return 0;
+}
